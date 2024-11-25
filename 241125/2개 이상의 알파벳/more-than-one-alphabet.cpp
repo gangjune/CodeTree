b0773f71b8bd207e6cfd1b5ac9374d9d5707dc86
23040502 <ererer1001@naver.com>
2024-11-25 23:06:31 +0900

    Title: 2개 이상의 알파벳, Time: 5ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 241125/2개 이상의 알파벳/more-than-one-alphabet.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <cstring>
+using namespace std;
+bool f(char A[], int size){
+    for(int i = 1; i < size - 1; i++){
+        for(int j = i; j < size; j++){
+            if(A[i-1] == A[j]) return true;
+        }
+    }
+    return false;
+}
+int main() {
+    char A[100];
+    cin >> A;
+    int size = strlen(A);
+    if(f(A, size)) cout << "Yes";
+    else cout << "No";
+    return 0;
+}
