50ea6c4fcedc2123bbddc10589eba8cd06681464
23040502 <ererer1001@naver.com>
2024-11-25 23:09:01 +0900

    Title: 2개 이상의 알파벳, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 241125/2개 이상의 알파벳/more-than-one-alphabet.cpp
+++ 241125/2개 이상의 알파벳/more-than-one-alphabet.cpp
@@ -4,7 +4,7 @@ using namespace std;
 bool f(char A[], int size){
     for(int i = 1; i < size - 1; i++){
         for(int j = i; j < size; j++){
-            if(A[i-1] == A[j]) return true;
+            if(A[i-1] != A[j]) return true;
         }
     }
     return false;
