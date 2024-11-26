ba913b092fc4b154fb4c0c4d7f011c4e7af72941
23040502 <ererer1001@naver.com>
2024-11-26 15:02:42 +0900

    Title: 재귀함수의 꽃, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 241126/재귀함수의 꽃/the-flower-of-the-recursive-function.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+void print(int N){
+    if(N==0) return;
+    cout << N << " ";
+    print(N-1);
+    cout << N << " ";
+}
+int main() {
+    int N;
+    cin >> N;
+    print(N);
+    return 0;
+}
