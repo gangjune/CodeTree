e29c3e8996b0ed576f982da3dba225c9023e5f05
23040502 <ererer1001@naver.com>
2024-11-28 17:06:07 +0900

    Title: 왔다 갔던 구역 2, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 241128/왔다 갔던 구역 2/area-been-to-and-from2.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n, x;
+    cin >> n;
+    int list[2000];
+    for(int i = 0; i < 2000; i++){
+        list[i] = 0;
+    }
+    char dir;
+    int loc = 1000;
+   // list[loc] = 1;
+    for(int i = 0; i < n; i++){
+        cin >> x >> dir;
+        if(dir == 'L'){
+            for(int j = loc-1; j >= loc-x; j--){
+                list[j]++;
+            }
+            loc -= x;
+        }
+        else if(dir == 'R'){
+            for(int j = loc+1; j <= loc+x; j++){
+                list[j]++;
+            }
+            loc += x;
+        }
+        else {
+            cout << "error";
+            return 0;
+        }
+    }
+    int cnt=0;
+    for(int i = 0; i < 2000; i++){
+        if(list[i] > 1) cnt++;
+    }
+    cout << cnt;
+    return 0;
+}
