7b6f862576b55a69548ebba46b85b46c9a9b5c2c
23040502 <ererer1001@naver.com>
2024-11-28 12:47:22 +0900

    Title: 100으로 나눈 나머지의 수열, Time: 1069ms, Memory: 0MB, Status: Time Limit Exceed - CodeTree

---
+++ 241128/100으로 나눈 나머지의 수열/sequence-of-remainder-divided-by-100.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int f(int x, int y, int n){
+    if(n==1) return y;
+    return f(y, (x*y)%100, n-1);
+}
+int main() {
+    int n;
+    cin >> n;
+    cout << f(2, 4, n-1);
+    return 0;
+}
