c75f345ac5256583c800fcb5aab3b85acf804d77
이강준 <ererer1001@naver.com>
2024-11-29 20:50:47 +0900

    Title: 1이 3개 이상 있는 위치, Time: 7ms, Memory: 0MB, Status: Passed - CodeTree

--- 241129/1이 3개 이상 있는 위치/place-more-than-3-ones.cpp
+++ 241129/1이 3개 이상 있는 위치/place-more-than-3-ones.cpp
@@ -14,10 +14,10 @@ int main() {
     for(int i = 0; i<n; i++){
         for(int j = 0; j<n; j++){
             cnt = 0;
-            if(map[j-1][i]==1) cnt++;
-            if(map[j+1][i]==1) cnt++;
-            if(map[j][i-1]==1) cnt++;
-            if(map[j][i+1]==1) cnt++;
+            if (j > 0 && map[i][j-1] == 1) cnt++; // 위쪽
+            if (j < n - 1 && map[i][j+1] == 1) cnt++; // 아래쪽
+            if (i > 0 && map[i-1][j] == 1) cnt++; // 왼쪽
+            if (i < n - 1 && map[i+1][j] == 1) cnt++; // 오른쪽
             if(cnt >= 3) numof3++;
         }
     }
