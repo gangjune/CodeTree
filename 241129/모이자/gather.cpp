6741add988f3928bf6ccb630eb25fdfb1eff43b5
이강준 <ererer1001@naver.com>
2024-11-29 22:33:03 +0900

    Title: 모이자, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 241129/모이자/gather.cpp
+++ 241129/모이자/gather.cpp
@@ -8,13 +8,13 @@ int main() {
     for(int i = 0; i<n; i++){
         cin >> man[i];
     }
-    int min = 10000;
+    int min;
     for(int i = 0; i<n; i++){   //i번째 집으로 모이는 경우들
         int sum=0;
         for(int j = 0; j<n; j++){   //j번째 집에서 오는 경우
             sum += abs(i-j)*man[j];    //거리 * 사람수
         }
-        if (min > sum) min = sum;
+        if (i == 0 || min > sum) min = sum; //min 초기값 정해주면서 비교
     }
     cout << min;
     return 0;
