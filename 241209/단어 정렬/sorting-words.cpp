b4cc53cf588b46444c650c75684992695d49c984
이강준 <ererer1001@naver.com>
2024-12-09 11:52:22 +0900

    Title: 단어 정렬, Time: 5ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 241209/단어 정렬/sorting-words.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <algorithm>
+#include <string>
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+    string str[n];
+    for(int i = 0; i<n; i++){
+        cin >> str[i];
+    }
+    sort(str, str+n);
+    for(int i = 0; i<n; i++){
+        cout << str[i] << endl;
+    }
+    return 0;
+}
